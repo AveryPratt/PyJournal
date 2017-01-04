c7d70c899d8080bb3b44343bcf4d9c54516d33c7
Avery Pratt <apratt91@gmail.com>
2017-01-03 21:55:48 -0800

    ready to implement html templates

--- learning_journal/views.py
+++ learning_journal/views.py
@@ -6,9 +6,24 @@ from pyramid.response import Response
 
 HERE = os.path.dirname(__file__)
 
-def home_page(request):
-    imported_text = open(os.path.join(HERE, 'sample.html')).read()
+def home(request):
+    imported_text = open(os.path.join(HERE, 'data/home.html')).read()
+    return Response(imported_text)
+
+def detail(request):
+    imported_text = open(os.path.join(HERE, 'data/detail.html')).read()
+    return Response(imported_text)
+
+def create(request):
+    imported_text = open(os.path.join(HERE, 'data/create.html')).read()
+    return Response(imported_text)
+
+def update(request):
+    imported_text = open(os.path.join(HERE, 'data/update.html')).read()
     return Response(imported_text)
 
 def includeme(config):
-    config.add_view(home_page, route_name='home')
+    config.add_view(home, route_name='home')
+    config.add_view(detail, route_name='detail')
+    config.add_view(create, route_name='create')
+    config.add_view(update, route_name='update')
